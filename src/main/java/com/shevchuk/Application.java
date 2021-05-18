9fd3c1c4d6418f71159234abd7507242c1c7578f
xo-Natsumi-ox <53785210+xo-Natsumi-ox@users.noreply.github.com>
2021-05-18 15:41:12 +0300

    Update Application.java

--- src/main/java/com/shevchuk/Application.java
+++ src/main/java/com/shevchuk/Application.java
@@ -2,16 +2,10 @@ package com.shevchuk;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Running Spring Boot Application");
     }
 }
